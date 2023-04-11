4ae93542c84f080845305334b674bf5153749496
Juan David Arcila <juandavidarcila18@gmail.com>
2023-04-11 06:37:36 -0500

    proyecto base

---
+++ src/main/java/com/restaurante/remy/RemyAplication.java
@@ -0,0 +1,14 @@
+package com.restaurante.remy;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemyAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RemyAplication.class, args);
+    }
+}
+
